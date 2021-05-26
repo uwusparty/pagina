d934d989ff0523bea3770dc3601264f73f11d400
Asier <asier123321@gmail.com>
2021-05-26 11:37:56 +0200

    Ranking terminado, errores cambiados, ...

--- index.php
+++ index.php
@@ -21,8 +21,8 @@
         <div id='centro'>
             <div class='apartado'>
                 <div>
-                    <h2>¿Qué es Trivialmi?</h2>
-                    <p>Trivialmi es el puto mejor juego 👊 👊 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam dolorem repellendus, dolor ullam harum beatae tempore et iste eaque officia asperiores! Porro laudantium aperiam vitae enim dolore repellat facere illum! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestiae, fugiat exercitationem natus placeat nihil eius eos neque pariatur consectetur nesciunt suscipit ullam molestias voluptates odio dolore nostrum numquam aspernatur? lore</p>
+                    <h2>¿Quiénes somos?</h2>
+                    <p>En UwU's Party Entertainment ponemos nuestro corazón y nuestra alma en todo lo que hacemos. Todos los días seguimos nuestros valores fundamentales para seguir creando experiencias épicas de entretenimiento para todos los jugadores. No importa quién eres ni dónde estás: ya seas miembro de nuestra vibrante y creciente comunidad, trabajes con nosotros en alguna de las oficinas o lleves un poco de UwU's Party contigo al espacio, te damos una cálida bienvenida.</p>
                 </div>
                 <div class='paraImagen'>
                     <img src='resources/ruleta.png' alt='Imagen de ruleta'>
@@ -34,12 +34,13 @@
                 </div>
                 <div>
                     <h2>¿Qué es Trivialmi?</h2>
-                    <p>Trivialmi es el puto mejor juego 👊 👊 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam dolorem repellendus, dolor ullam harum beatae tempore et iste eaque officia asperiores! Porro laudantium aperiam vitae enim dolore repellat facere illum! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestiae, fugiat exercitationem natus placeat nihil eius eos neque pariatur consectetur nesciunt suscipit ullam molestias voluptates odio dolore nostrum numquam aspernatur? lore</p>
+                    <!-- <p>Trivialmi es un juego de preguntas de un solo jugador totalmente gratuito. </p> --> 
+                    <p>Trivialmi es un juego de preguntas no lineal en el que escribes tus propias preguntas sin limitarte a seguir un camino prefijado. Participa en un amplio mundo abierto con seis categorías únicas, todo cuanto hagas tendrá su repercusíon en el mundo. Con sus preguntas orientadas al jugador de Trivialmi los jugadores crean practicamente todo tipo de preguntas a partir de los recursos que consiguen.</p>
                 </div>
             </div>
             <div class='apartado'>
                 <div>
-                    <h2>¿Qué es Trivialmi?</h2>
+                    <h2>¡Únete a nosotros!</h2>
                     <p>Trivialmi es el puto mejor juego 👊 👊 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam dolorem repellendus, dolor ullam harum beatae tempore et iste eaque officia asperiores! Porro laudantium aperiam vitae enim dolore repellat facere illum! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestiae, fugiat exercitationem natus placeat nihil eius eos neque pariatur consectetur nesciunt suscipit ullam molestias voluptates odio dolore nostrum numquam aspernatur? lore</p>
                 </div>
                 <div class='paraImagen'>
